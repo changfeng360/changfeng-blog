781495134cb563db387fbc4e26a6907464c2004e
Chang796 <3113129562@qq.com>
2026-08-04 17:27:52 +0800

    fix: enable static export to bypass asset size limit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { fileURLToPath } from "node:url";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   devIndicators: false,
   async redirects() {
